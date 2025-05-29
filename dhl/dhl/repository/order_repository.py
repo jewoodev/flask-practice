0258b614fac96a7371e3bcc2e314bc2dceab4bba
jewoodev <jewoos15@naver.com>
2025-05-29 14:50:49 +0900

    📝 update: 두 배송 업체에 따른 로직 수행을 위한 테이블 구조 수정

--- dhl/dhl/repository/order_repository.py
+++ dhl/dhl/repository/order_repository.py
@@ -1,11 +1,28 @@
+from datetime import datetime
+from typing import Iterable
+
 from dhl import db
 from dhl.models import Order
 
-def insert(order):
+def insert(order: Order):
     return db.session.add(order)
 
-def find_by_id(id):
+def insert_all(orders: Iterable[Order]):
+    return db.session.add_all(orders)
+
+def find_by_id(id: int):
     return db.session.query(Order).filter(Order.id == id).first()
+
+def find_by_order_status_and_after(order_status: str, start_datetime: datetime):
+    return db.session.query(Order).filter(
+        Order.order_status == order_status,
+        Order.created_at >= start_datetime
+    )
+
+def find_by_waybill_number(waybill_number: int):
+    return db.session.query(Order).filter(
+        Order.waybill_number == waybill_number
+    ).first()
 
 def delete_all():
     return db.session.query(Order).delete()
@@ -18,3 +35,6 @@ def rollback():
 
 def flush():
     return db.session.flush()
+
+def close():
+    return db.session.close()
