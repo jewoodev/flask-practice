2fe53ba4916b6ac11b05b45b778f8880affdfb1c
jewoodev <jewoos15@naver.com>
2025-05-30 14:13:12 +0900

    📝 update: solve issue #3

--- dhl/dhl/service/dto.py
+++ dhl/dhl/service/dto.py
@@ -3,13 +3,12 @@ from dataclasses import dataclass
 
 @dataclass
 class FedExDTO:
-    trackingNumbers: list[str]
+    trackingPairs: dict[int] # 운송장 번호를 Key로, 그 운송장 번호에 매칭되는 Order의 id를 Value로 갖는 dict
     beginDate: str
     endDate: str
-    orderIds: list[int]
 
 @dataclass
 class DHLDTO:
-    shipmentTrackingNumber: list[str]
+    trackingPairs: dict[int] # 운송장 번호를 Key로, 그 운송장 번호에 매칭되는 Order의 id를 Value로 갖는 dict
     levelOfDetail: str
     trackingView: str
