d8036ddf3385f4461cd9f397b29440c13a068c15
jewoodev <jewoos15@naver.com>
2025-05-30 12:30:45 +0900

    📰 feat: flask-migrate 도입

--- dhl/dhl/__init__.py
+++ dhl/dhl/__init__.py
@@ -1,12 +1,15 @@
 from flask import Flask
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-from dhl.config import LocalConfig
+from dhl.config import TestConfig
 from dhl.views.main_views import bp as main_bp
 
 import logging
 
+
 db = SQLAlchemy()
+migrate = Migrate()
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -19,14 +22,16 @@ def create_app(configuration=None):
 
     # 설정 적용
     if configuration is None:
-        app.config.from_object(LocalConfig)
+        app.config.from_object(TestConfig)
     else:
         app.config.from_object(configuration)
 
-    ## db instance initialize
-    db.init_app(app)
-
     # 블루프린트 등록
     app.register_blueprint(main_bp)
+
+    ## db instance initialize
+    db.init_app(app)
+    migrate.init_app(app, db)
+    from . import models
 
     return app
